e1ace9d37d06c728b7959dad8d0c0300d602f233
GalihRakasiwhi <galih.rakasiwhi@gmail.com>
2023-04-12 18:02:14 +0700

    Update getListDay.js

--- getListDay.js
+++ getListDay.js
@@ -1,17 +1,3 @@
-/*  Cara pakai: 
-*   const getListDay = require('./getListDay');
-*   let startDate = new Date("2023-04-12"); // example
-*   let endDate = new Date("2023-05-12"); // example
-*   getListDay(startDate, endDate);
-*
-*   ex - adalah list array berisi nomor hari yang akan di exlude
-*   0 = Minggu, 1 = Senin, ..., 6 = Sabtu
-*   nilai default pada ex adalah [0, 6], alias [Minggu, Sabtu]
-*   nilai pada ex bisa angka 0 sampai 6, dengan jumlah list maksimal bisa berapa saja
-*   totalShowDay = total hari yang ditampilkan
-*   totalExDay = total haru yang tidak ditampilkan
-*/
-
 const getListDay = (startDate, endDate, ex = [0, 6]) => {
     const nameDay = ["Minggu","Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu"];
     let totalExDay = 0;
